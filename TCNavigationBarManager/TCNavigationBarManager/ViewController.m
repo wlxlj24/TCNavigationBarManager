e84e22d78c65a967bd079175d9d7dc7076c110f1
TailC <wlxlj24@qq.com>
2016-06-12 16:11:11 +0800

    导航栏透明渐变效果

--- TCNavigationBarManager/TCNavigationBarManager/ViewController.m
+++ TCNavigationBarManager/TCNavigationBarManager/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource,UIScrollViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic,readwrite,strong) UIImageView *barImageView;
 
 @end
 
@@ -16,12 +19,48 @@
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+	self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
+	[self.navigationController.navigationBar setTitleTextAttributes:@{NSForegroundColorAttributeName : [UIColor whiteColor]}];
+	self.navigationController.navigationBar.barTintColor = [UIColor redColor];
+	self.barImageView = self.navigationController.navigationBar.subviews.firstObject;
+	
+	[self.navigationController.navigationBar setBackgroundImage:self.barImageView.image
+												  forBarMetrics:UIBarMetricsDefault];
+	self.navigationController.navigationBar.shadowImage = [UIImage new];
+	
 }
 
 - (void)didReceiveMemoryWarning {
 	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
+}
+
+#pragma mark <UITableViewDelegate><UITableViewDataSource>
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+	return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+	return 100;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+	return 44.0f;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+	cell.textLabel.text = [NSString stringWithFormat:@"%ld",(long)indexPath.row];
+	return cell;
+}
+
+#pragma mark <UIScrollViewDelegate>
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+	CGFloat minAlphaOffset = - 64;
+	CGFloat maxAlphaOffset = 200;
+	CGFloat offset = scrollView.contentOffset.y;
+	CGFloat alpha = (offset - minAlphaOffset) / (maxAlphaOffset - minAlphaOffset);
+	_barImageView.alpha = alpha;
 }
 
 @end
