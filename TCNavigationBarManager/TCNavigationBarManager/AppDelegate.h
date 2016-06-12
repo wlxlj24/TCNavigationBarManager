2510fcc20caa40efbfa45acdc0bf76bb0dd8336c
TailC <wlxlj24@qq.com>
2016-06-12 16:10:58 +0800

    导航栏透明 渐变效果

---
+++ TCNavigationBarManager/TCNavigationBarManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TCNavigationBarManager
+//
+//  Created by TailC on 16/6/12.
+//  Copyright © 2016年 TailC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TCNavigationBarManager/TCNavigationBarManagerTests/TCNavigationBarManagerTests.m
@@ -0,0 +1,39 @@
+//
+//  TCNavigationBarManagerTests.m
+//  TCNavigationBarManagerTests
+//
+//  Created by TailC on 16/6/12.
+//  Copyright © 2016年 TailC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TCNavigationBarManagerTests : XCTestCase
+
+@end
+
+@implementation TCNavigationBarManagerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
